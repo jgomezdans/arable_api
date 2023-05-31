0281b7d66f3f6a716785ee416e9d4a097fc80810
Jose Gomez-Dans <jose.gomez-dans@kcl.ac.uk>
2023-05-31 17:37:54 +0100

    Can instruct dates to download
    This is useful to get the first version of the database going,
    just run the script for a bunch of days to populate the folder
    with the data up to now, and then everything gets downloaded by
    the cronjob

--- src/arable_api/__main__.py
+++ src/arable_api/__main__.py
@@ -6,14 +6,23 @@ from pathlib import Path
 @click.command()
 @click.option('-f', '--folder', default="./",
                help='Folder where to save CSV files')
-def main(folder):
-    files  = [f for f in  Path(folder).glob("*.csv")]
-    if not files: # No files, start downloading yesterday
-        gather_data(folder)
+@click.option('-d', '--date', default=None,
+               help='Date to gather data')
+def main(folder, date):
+    if date is not None:
+        try:
+            date = dt.datetime.strptime(date, "%Y-%m-%d")
+        except ValueError:
+            print(f"date option should be given as 'YYYY-MM-DD'")
+        gather_data(folder, start_time=date)
     else:
-        dates = sorted([dt.datetime.strptime(f.name.split("_")[0], "%Y-%m-%d")
-                 for f in files])
-        # find the latest date available and start on the following day
-        gather_data(folder, start_time=dates[-1] + dt.timedelta(days=1))
+        files  = [f for f in  Path(folder).glob("*.csv")]
+        if not files: # No files, start downloading yesterday
+            gather_data(folder)
+        else:
+            dates = sorted([dt.datetime.strptime(f.name.split("_")[0], "%Y-%m-%d")
+                    for f in files])
+            # find the latest date available and start on the following day
+            gather_data(folder, start_time=dates[-1] + dt.timedelta(days=1))
 
 main()
